a50e9bf9a2425d980763c4eabe762de073c37a91
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2021-02-26 19:52:28 +0000

    Bug fixed - Array not overwritten - Random working as array fixed

--- Assignment.java
+++ Assignment.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class GardenItemStore {
     Map<String, ArrayList<String>> map;
+    ArrayList<String> plantArrayList = new ArrayList<String>();
 
     //$ Constructor with no arguments which initialized hash map
     public GardenItemStore() {
@@ -15,20 +16,20 @@ class GardenItemStore {
 
     //$ Method that creates new mapping without replacing existing mapping
     public void put(String key, String plant) {
-        this.map.put(key, new ArrayList<String>()); // Create map with one key and empty array 
-        this.map.get(key).add(plant); // Get the map first and then add element to array
-        // System.out.println(this.map.get(key));
+        map.put(key, this.plantArrayList); // Create map with one key and empty array as list of plants
+        map.get(key).add(plant); // Get the map first and then add element to array
+        // System.out.println(this.map.get(key)); 
     }
     //$ Method that return a random value from a given key
     public String getRandomItem(String key) { //* Returns random plant by randomly selecting index
         Random randomGenerator = new Random();
         ArrayList<String> array = this.map.get(key); // Returns that array of plants
         int randomIndex = randomGenerator.nextInt(array.size()); // Select random index between 0 and array size
-        return (array.get(randomIndex)); //! BUG: Return last item in the array 
+        return (array.get(randomIndex)); 
     }
 
-    public String toString() {
-        return ("Array");
+    public void print(String key) {
+        System.out.println(this.map.get(key));
     }
 }
 
@@ -45,5 +46,7 @@ public class Assignment {
         System.out.println(foo.containsKey("3"));
 
         System.out.println(foo.getRandomItem("1"));
+
+        foo.print("1");
     }   
 }
