14ca3849464207ee5352d7aa377a75571419b851
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2021-02-27 20:54:24 +0000

    getRandom complete - Capitilise first letter of the map - Only return random if key exists - Improved testing -- Added getValue method to return values associated with key -- Added getSet method to return list of sets -- Added myTest method to test custom tests -- Added plantTest method to test university provided tests
    ! BUG: Mappings with different keys are added to the same bucket

--- Assignment.java
+++ Assignment.java
@@ -1,52 +1,103 @@
+
+//^ LIBRARIES:
 import java.util.*;
 
+//^ CLASSES:
 class GardenItemStore {
+    //^ Fields:
     Map<String, ArrayList<String>> map;
     ArrayList<String> plantArrayList = new ArrayList<String>();
 
-    //$ Constructor with no arguments which initialized hash map
+    //^ Methods
+    //$ Constructor that takes no arguments and initializes the map using a new
     public GardenItemStore() {
         map = new HashMap<String, ArrayList<String>>(); //* HashMap cannot have multiple values per key which means that an Array has to be used
     }
-
-    //$ Method that checks contains whether it contains a key
+    
+    //$ Checks whether the map contains a given string as a key
     public boolean containsKey(String key) {
         return (this.map.containsKey(key));
     }
 
-    //$ Method that creates new mapping without replacing existing mapping
-    public void put(String key, String plant) {
+    //£ Method that creates new mapping without replacing existing mapping
+    public void put(String key, String plant) { //! BUG: Stores items with different keys in the same bucket 
         map.put(key, this.plantArrayList); // Create map with one key and empty array as list of plants
         map.get(key).add(plant); // Get the map first and then add element to array
-        // System.out.println(this.map.get(key)); 
-    }
-    //$ Method that return a random value from a given key
-    public String getRandomItem(String key) { //* Returns random plant by randomly selecting index
-        Random randomGenerator = new Random();
-        ArrayList<String> array = this.map.get(key); // Returns that array of plants
-        int randomIndex = randomGenerator.nextInt(array.size()); // Select random index between 0 and array size
-        return (array.get(randomIndex)); 
     }
 
-    public void print(String key) {
-        System.out.println(this.map.get(key));
+    //$ Method that return a random value from a given key
+    public String getRandomItem(String key) { //* Returns random plant by randomly selecting index
+        //$ If key contains mapping then return random
+        if (this.map.get(key) != null) {
+            //£ Capitalize key 
+            Random randomGenerator = new Random();
+            ArrayList<String> array = this.map.get(key); // Returns that array of plants
+            int randomIndex = randomGenerator.nextInt(array.size()); // Select random index between 0 and array size
+            
+            String capitalPlant =  array.get(randomIndex).substring(0, 1).toUpperCase() + array.get(randomIndex).substring(1); // Capitalize first letter: Return first character and capitalize then add rest of string except first index
+            String capitalKey = key.substring(0, 1).toUpperCase() + key.substring(1);
+            
+            return (capitalKey + capitalPlant);
+        } else {
+            //$ If key does not contains mapping then return null 
+            return (null);
+        }       
+    }
+
+    //^ Tests
+    public ArrayList<String> getValue(String key) {
+        return (this.map.get(key));
+        // System.out.println(this.map.keySet() + this.map.get(this.map.keySet()));
+    }
+    public void getSet() {
+        System.out.println(this.map.keySet());
     }
 }
 
 public class Assignment {
+    //^ Methods:
+    public static void myTest() {
+        GardenItemStore test = new GardenItemStore();
+        
+        test.put("one", "one1");
+        test.put("one", "one2");
+        test.put("one", "one3");
+        test.put("one", "one4");
+        test.put("two", "Two1");
+        test.put("two", "two2");
+        test.put("three", "Three");
+
+        System.out.println("Contains 1: " + test.containsKey("one"));
+        System.out.println("Contains 2: " + test.containsKey("two"));
+        System.out.println("Contains 3: " + test.containsKey("three"));
+        System.out.println("Contains 4: " + test.containsKey("four")); System.out.println();
+
+        System.out.println("Get Values 1:" + test.getValue("two"));
+        System.out.println("Get Values 2:" + test.getValue("one"));
+        System.out.println("Get Values 3:" + test.getValue("three"));System.out.println();
+
+        System.out.println("Random Item 1:" + test.getRandomItem("one"));
+        System.out.println("Random Item 2:" + test.getRandomItem("two"));
+        System.out.println("Random Item 3:" + test.getRandomItem("three"));System.out.println();
+    }
+
+    public static void plantsTest() {
+        GardenItemStore gis0 = new GardenItemStore();
+        
+        gis0.put("a", "azalea");
+        gis0.put("bu", "burning bush");
+        gis0.put("bu", "bursting heart");
+        gis0.put("a", "amur chokecherry");
+
+        System.out.println(gis0.containsKey("a"));
+
+        System.out.println(gis0.getRandomItem("a"));
+
+        gis0.print("a");
+    }
+
     public static void main(String[] args) {
-        GardenItemStore foo = new GardenItemStore();
-        foo.put("1", "plant");
-        foo.put("1", "Test");
-        foo.put("1", "Hello");
-        foo.put("1", "World");
-        foo.put("1", "Table");
-
-        System.out.println(foo.containsKey("1"));
-        System.out.println(foo.containsKey("3"));
-
-        System.out.println(foo.getRandomItem("1"));
-
-        foo.print("1");
+        myTest();
+        // plantsTest();
     }   
 }
